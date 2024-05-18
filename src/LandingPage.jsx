2288e265045f0ecf0a0f6678e66203ad6e010ae8
Pranav13072005 <pranavjaiganesh2005@gmail.com>
2024-05-18 21:48:46 +0530

    initial

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import CountUp from 'react-countup';
 
 function Navbar() {
     return (
@@ -23,19 +24,19 @@ function Navbar() {
         <div className="features-container">
           <div className="students">
           <FontAwesomeIcon icon={faGraduationCap} className="student"/>
-          <p className="numbers">20k+</p>
+          <p className="numbers"><CountUp start={0} end={200} duration = {2} useEasing={true}/>k+</p>
           <p className="numbers-header">STUDENTS</p>
-          <p className="features-desc">Within a month of starting we have such a high number of students already with us</p>
+          <p className="features-desc">Join a thriving community of over 20,000 aspiring developers!</p>
           </div>
           <div className="students">
           <FontAwesomeIcon icon={faCode} className="student"/>
-          <p className="numbers">180+</p>
+          <p className="numbers"><CountUp start={0} end={180} duration = {2}/>+</p>
           <p className="numbers-header">COURSES</p>
           <p className="features-desc">Explore 180+ expertly crafted courses to elevate your skills and become a full stack developer!</p>
           </div>
           <div className="students">
           <FontAwesomeIcon icon={faThumbsUp} className="student"/>
-          <p className="numbers">1000+</p>
+          <p className="numbers"><CountUp start={0} end={1000} duration={1.5}/>+</p>
           <p className="numbers-header">RECRUITMENTS</p>
           <p className="features-desc">Join us and be one of the 2,500+ students who have landed their dream jobs!</p>
           </div>
