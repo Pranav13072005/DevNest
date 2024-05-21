bbdfce48f2808f4ef945d5f6fa24995760838dad
Pranav13072005 <pranavjaiganesh2005@gmail.com>
2024-05-21 13:36:36 +0530

    about1

--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -11,8 +11,16 @@ function AboutPage() {
         <div className='Navbar'>
         <Navbar/>
         </div>
-        
-      THIS IS THE ABOUT PAGE
+        <div className='section-1'>
+          <div className='section-1-left'>
+            <p className='section-1-left-header'>About <span className='section-1-left-header-us'>Us</span></p>
+            <p className='section-1-left-text'>" Welcome to DevNest, your hub for innovative online courses designed to elevate your skills! Scroll down to explore our key features, or press "Our Story" to read about our journey. "</p>
+          </div>
+          <div className='section-1-right'>
+            <p className='our-story-desc'></p>
+            <button className='our-story'>Our Story</button>
+          </div>
+        </div>
     </div>
   )
 }
